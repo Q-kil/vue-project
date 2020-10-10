f530e14467c67de7d990449a55e81314991f1349
KilFront <kaifawebb@gmail.com>
2020-10-10 14:58:15 +0800

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <!-- <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -26,16 +26,76 @@
       <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    </ul> -->
+    <!-- <div  @click="initializeGame"  class="flex" >
+      <a  href="#1"  class="btn">Initialize</a>
+    </div> -->
+    <ion-phaser
+      v-bind:game.prop='game'
+      v-bind:initialize.prop='initialize'
+    />
+    <div>
+      <input type="text">
+    </div>
   </div>
 </template>
 
 <script>
+import Phaser from "phaser";
 export default {
   name: 'HelloWorld',
+  data() {
+    return {
+      initialize: false,
+      game: {
+        width: "100%",
+        height: "80%",
+        type: Phaser.AUTO,
+        scene: {
+          preload() {
+              this.load.plugin('rexinputtextplugin', 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexinputtextplugin.min.js', true); 
+          },
+          init() {
+            this.cameras.main.setBackgroundColor("#24252A");
+          },
+          create() {
+            // this.helloWorld = this.add.text(
+            //   this.cameras.main.centerX,
+            //   this.cameras.main.centerY,
+            //   "Hello World",
+            //   { font: "40px Arial",  fill: "#ffffff" }
+            // );
+            // this.helloWorld.setOrigin(0.5);
+
+
+            var inputText = this.add.rexInputText(100, 500, 200, 60, {
+            type: 'textarea',
+            text: 'hello world',
+            fontSize: '20px',
+           })
+          },
+          // update() {
+          //   this.helloWorld.angle += 1;
+          // }
+        },
+        dom: {
+           createContainer: true
+        }
+      }
+    };
+  },
   props: {
     msg: String
+  },
+  mounted(){
+    this.init()
+  },
+  methods: {
+    init() {
+      this.initialize = true;
+    }
   }
+
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { defineCustomElements as defineIonPhaser } from '@ion-phaser/core/loader'
 
 Vue.config.productionTip = false
+
+defineIonPhaser(window)
 
 new Vue({
   render: h => h(App)
 }).$mount('#app')
+
+
+// 增加cordova文件
+if (window.location.protocol === 'file:') {
+  let cordovaScript = document.createElement('script')
+  cordovaScript.setAttribute('type', 'text/javascript')
+  cordovaScript.setAttribute('src', 'cordova.js')
+  document.body.appendChild(cordovaScript)
+}
