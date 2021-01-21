b88eef002ec2cade938a73787f2d012bb0082ddc
kaifawebb@gmail.com <niekaifa@apowo.com>
2021-01-21 15:20:25 +0800

    height

--- src/main.js
+++ src/main.js
@@ -6,3 +6,6 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App)
 }).$mount('#app')
+
+console.log('innerHeight', window.innerHeight);
+console.log('clientHeight', document.documentElement.clientHeight);
